6790fe6319685f3b2ac9601e1b75354107ed76dd
rojaahmed <rojaahmed21@gmail.com>
2025-09-11 23:37:23 +0300

    Add latest changes to new branch

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -8,15 +8,11 @@
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
-             <li class="nav-item">
-  <a href="{{ route('home') }}#about" class="nav-link">About</a>
-</li>
+                <li class="nav-item"><a href="{{ route('home') }}#about" class="nav-link">About</a></li>
                 <li class="nav-item"><a href="{{route('courses')}}" class="nav-link">Course</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Instructor</a></li>
                 <li class="nav-item"><a href="{{ route('blog.index') }}" class="nav-link">Blog</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-
-
 
                 {{-- Giriş yapmamış kullanıcılar için linkler --}}
                 @guest
@@ -24,8 +20,15 @@
                     <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
                 @endguest
 
-
-
+                {{-- Giriş yapmış kullanıcılar için Logout --}}
+                @auth
+                    <li class="nav-item">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="nav-link btn btn-link" style="display:inline; padding:0; color:white;">Logout</button>
+                        </form>
+                    </li>
+                @endauth
             </ul>
         </div>
     </div>
